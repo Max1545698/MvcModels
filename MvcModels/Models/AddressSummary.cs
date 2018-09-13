d1b3060e0205ea0894aeeacde338770c340d720e
Максим Рожок <max15071999@gmail.com>
2018-09-13 14:19:32 +0300

    Add project files.

---
+++ MvcModels/Models/AddressSummary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MvcModels.Models
+{
+    public class AddressSummary
+    {
+        public string City { get; set; }
+        //[BindNever]
+        public string Country { get; set; }
+    }
+}
